875dfa0821b706644ff9287d9e88510f8a9c3020
bestswlkh0310 <hhhello0507@dgsw.hs.kr>
2024-06-08 15:43:48 +0900

    Singleton

---
+++ singleton.py
@@ -0,0 +1,16 @@
+class Singleton:
+    __instance = None
+
+    @classmethod
+    def __getInstance(cls):
+        return cls.__instance
+
+    @classmethod
+    def instance(cls):
+        cls.__instance = cls()
+        cls.instance = cls.__getInstance
+        return cls.__instance
+
+
+class MyClass(Singleton):
+    pass
