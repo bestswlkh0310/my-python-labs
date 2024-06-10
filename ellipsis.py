0ddf8d93df0185a130013ad88ec706dd97d39073
bestswlkh0310 <hhhello0507@dgsw.hs.kr>
2024-06-10 18:17:27 +0900

    Ellipsis

---
+++ ellipsis.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def a():
+    ...
+
+
+arr = np.array([1, 2, 3, 4], dtype=np.int64)
+print(arr[1, ...])
+
+print(...)
+
+print([1, 2, 3, 4, 5, 6])
+
+b = [0, 1]
+b[0] = b
+
+print(b)
